9dc0458b1aa6730d61370b7166ce5ea1f324ea0e
hcgustavo <ghcaixeta@gmail.com>
2016-09-21 23:26:36 -0300

    Update config.js
    Removed production details

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret': 'quebecesttressympa',
-  'database': 'mongodb://heroku_zrnv1k9w:74sa7nvs0lvjcabmdhtghirp7k@ds013956.mlab.com:13956/heroku_zrnv1k9w'
+  'secret': 'ANY_STRING_HERE',
+  'database': 'DATABASE_URI'
 };
